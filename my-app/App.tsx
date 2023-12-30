7cb970f96cee8ad90ba72e2f9fe78467290bc932
childevnote <daniel011125@gmail.com>
2023-12-31 01:12:21 +0900

    Install: TailwindCSS

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
   console.log('App executed');
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#797979',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
