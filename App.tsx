55ca8e12b956465e50a7dec74d92a35628eaafab
childevnote <daniel011125@gmail.com>
2024-01-03 01:41:30 +0900

    Add: TouchableOpacity

--- App.tsx
+++ App.tsx
@@ -1,19 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
 
 export default function App() {
+  const now =  new Date().toLocaleTimeString();
   return (
     <View style={styles.root}>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
       <StatusBar style="auto" />
+      <TouchableOpacity activeOpacity={0.5}>
+        <Text style={styles.text}>
+          Open up App.tsx to start working on your app!
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.5}>
+        <Text style={styles.text}>
+          Open up App.tsx to start working on your app!
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.5}>
+        <Text style={styles.text}>
+          Open up App.tsx to start working on your app!
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.5}>
+        <Text style={styles.text}>
+          {now}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -24,11 +37,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+
   },
   text: {
     fontSize: 24,
     backgroundColor: "purple",
     borderRadius: 20,
+    overflow: "hidden",
     padding: 10,
     margin: 10,
     fontWeight: "bold",
